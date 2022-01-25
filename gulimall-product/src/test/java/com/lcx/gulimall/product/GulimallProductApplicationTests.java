320d6c23e314461afed0d61f0797017b4c4dcb44
BadData <starshineforyou@163.com>
2022-01-25 17:49:13 +0800

    初始架构

---
+++ gulimall-product/src/test/java/com/lcx/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcx.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
