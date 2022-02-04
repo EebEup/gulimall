f054911f32eba2e1da29151889e574320281c8b8
BadData <starshineforyou@163.com>
2022-02-04 09:57:25 +0800

    初始架构

--- gulimall-coupon/src/main/java/com/lcx/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/lcx/gulimall/coupon/controller/CouponController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,11 +27,30 @@ import com.lcx.common.utils.R;
  * @email 2251727682@qq.com
  * @date 2022-01-26 10:05:33
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${ }")
+    private Integer age;
+
+
+    @RequestMapping("/test")
+    public R test(){
+        return R.ok().put("name",name).put("age",age);
+
+    }
+
+    @RequestMapping("/member/list")
+    public R membercoupons(){
+        CouponEntity couponEntity = new CouponEntity();
+        couponEntity.setCouponName("on sale discount 7");
+        return R.ok().put("coupons",Arrays.asList(couponEntity));
+    }
 
     /**
      * 列表
--- gulimall-member/src/main/java/com/lcx/gulimall/member/controller/MemberController.java
+++ gulimall-member/src/main/java/com/lcx/gulimall/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
+import com.lcx.gulimall.member.feign.CouponFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,9 +32,22 @@ public class MemberController {
     @Autowired
     private MemberService memberService;
 
+    @Autowired
+    CouponFeignService couponFeignService;
     /**
      * 列表
      */
+
+    @RequestMapping("/coupons")
+    public R test(){
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setNickname("lcx");
+        R membercoupons = couponFeignService.membercoupons();
+        return R.ok().put("member",memberEntity).put("coupon",membercoupons.get("coupons"));
+
+
+    }
+
     @RequestMapping("/list")
    // @RequiresPermissions("member:member:list")
     public R list(@RequestParam Map<String, Object> params){
